5b7d6c8708440e4a8279d51dfa734a7608f6f3e1
gordem1074 <gordem1074@users.noreply.github.com>
2021-02-15 12:05:35 +0200

    v1.2

--- Exercise/ex4/main.cpp
+++ Exercise/ex4/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	float sum, month, result;
+	float sum, month, result, profit;
+	cout << " Deposit Calculator"  << endl;
 	cout << "Input sum in \"$\" and month:"  << endl;
 	cin >> sum >> month;
-	result = sum *(5/100)/12/month;
-	cout << " Result: " << result << endl;
 	
-	return 0;
+	result = sum *(pow( (1 + 5.0/100 ), month/12) );
+	profit = (result - sum)/month;
+	
+	cout 
+	<< "The profit from the deposit for the month: "<< profit << "$" << endl
+	<< "Entire term of the deposit: " << month << " month" << endl
+	<< "Your balance: " << result << "$" << endl;
+	
+	return result;
 }
